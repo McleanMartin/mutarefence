58255a580e286cce45c0f7ffd7e4ccb5d71b0cbf
McleanMartin <chikadaymartin@gmail.com>
2026-05-04 12:45:45 +0200

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Mutare Fence and Gates";
+export const SITE_DESCRIPTION = "Mutare Fence and Gates is the local leader in wire products and fencing solutions, delivering trusted security and perimeter protection.";
